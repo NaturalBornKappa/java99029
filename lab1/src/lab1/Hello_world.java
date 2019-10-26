70e47bfeb31dcd80fae1df667bb09b4f5af14046
99029 <99029@U-55172.domena.uksw.edu.pl>
2019-10-26 12:24:46 +0200

    Not so funny commit message

---
+++ lab1/src/lab1/Hello_world.java
@@ -0,0 +1,20 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class Hello_world {
+
+	Scanner scan = new Scanner(System.in);
+	
+	public static void main(String []Args) {
+
+	System.out.print("Hello World!");
+	
+	String hello = "Hello World";
+	
+	System.out.print(hello);
+	
+	System.out.print("Napis \"Hello World\" ma " + hello.length() + " znaków");
+	
+	}
+}
